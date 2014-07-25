bb1f68891fa35a434b2dc560d2d840eae9972145
Paul Klopping <au70pi1337@gmail.com>
2014-07-24 22:33:51 -0500

    updates

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,3 @@
-<img src="http://placekitten.com/800/800" />
-<img src="http://placebear.com/800/800" />
+<img src="http://placekitten.com/400/800" />
+<img src="http://placebear.com/800/400" />
 <img src="http://placecage.com/800/800" />
